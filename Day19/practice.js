d537242156c2e4bf8f938fbd39479b1c07318099
Anil <anilnepal14949@gmail.com>
2022-09-14 20:38:24 +1000

    Day 19 Contents and Exercises

---
+++ Day19/practice.js
@@ -0,0 +1,77 @@
+// var myArray = ["item1", "item2", "item3"];
+
+// var myArray = new Array("item1", "item2", "item3");
+// var myArray1 = new Array("item4", "item5", "item2");
+
+// var mergedArray = myArray1.concat(myArray);
+// myArray.concat();
+// var sortedArray = myArray.sort();
+
+// console.log(mergedArray);
+// console.log(sortedArray);
+
+// var numArray = [20, 4, 5, 10, 7, -10];
+
+// var sum = 0;
+// for(index in numArray) {
+// 	document.write(numArray[index]+"<br>");
+// 	sum += numArray[index]; // sum = sum + numArray[index]
+// }
+// document.write("The sum of array elements is "+ sum);
+
+// var sortedArray = numArray.sort(function(a, b) {
+// 	return b - a;
+// });
+
+// var max = Math.max.apply(null, numArray);
+// var min = Math.min.apply(null, numArray);
+
+
+// var person = {
+// 	"first_name": "alkdf",
+// 	"last_name": "aljdflkajl",
+// 	"gender": "adjfla",
+// 	"isPerson": true,
+// 	// "numArray": numArray
+// }
+
+// for(key in person) {
+// 	console.log(person[key]+"<br>");
+// 	// document.write(person);
+// }
+
+// person.first_name = "John";
+// person.something = "something";
+
+// delete person.something;
+
+// console.log(person);
+
+// var i = 1;
+
+// while(i < 5) {
+// 	document.write(i);
+
+// 	i++;
+// }
+
+// var j = 1;
+
+// do {
+// 	document.write(j);
+
+// 	j+=2;
+// } while(j < 5);
+
+// for (var i = 1; i <= 5; i++) {
+// 	document.write(i);
+// }
+
+// function sum(num1, num2) {
+// 	var sum = num1 + num2;
+// 	document.write("The sum is "+ sum);
+// }
+
+// sum(10, 15);
+// sum(25, 26);
+// sum(59, 18);
