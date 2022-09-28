be5c02e52e46a224bddf28ef2f17387de1a6d9a7
Anil <anilnepal14949@gmail.com>
2022-09-28 21:32:21 +1000

    Day25 Contents and Exercises

---
+++ Day25/emoji-rating/script.js
@@ -0,0 +1,27 @@
+const starsEl = document.querySelectorAll(".fa-star");
+const emojisEl = document.querySelectorAll(".emoji-container .far");
+
+const colorsArr = ["red", "orange", "lightblue", "lightgreen", "green"];
+
+let updateRating = index => {
+	starsEl.forEach((starEl, idx) => {
+		if(idx <= index) {
+			starEl.classList.add("active");
+		} else {
+			starEl.classList.remove("active");
+		}
+	});
+
+	emojisEl.forEach((emojiEl) => {
+		emojiEl.style.transform = `translateX(-${index * 50}px)`;
+		emojiEl.style.color = colorsArr[index];
+	});
+}
+
+starsEl.forEach((starEl, index) => {
+	starEl.addEventListener("click", () => updateRating(index));
+});
+
+updateRating(0);
+
+
