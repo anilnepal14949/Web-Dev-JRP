8bfb74ce31494ff1c86145f879e7105226b99b47
Anil <anilnepal14949@gmail.com>
2022-11-09 20:07:12 +1100

    Updated CRUD project

--- Day39/php/index.php
+++ Day39/php/index.php
@@ -2,10 +2,13 @@
 	require_once 'database.php';
 	$message = "";
 
-	$name = $email = "";
+	$name = $email = $id = "";
 
-	$type = $_GET["type"] ?? $_GET["type"]; 
-	$id = $_GET["id"] ?? $_GET["id"]; 
+	$type = isset($_GET["type"])?$_GET["type"]:''; 
+	$id = isset($_GET["id"])?$_GET["id"]:''; 
+	$search = isset($_GET["search"])?$_GET["search"]:''; 
+	$sort = isset($_GET["sort"])?$_GET["sort"]:''; 
+	$limit = isset($_GET["limit"])?$_GET["limit"]:''; 
 
 	if(isset($_POST["saveBtn"])) {
 		$name = $_POST["name"];
@@ -20,10 +23,13 @@
 	}
 
 	if(isset($_POST["updateBtn"])) {
-		$name = $_POST["name"];
-		$email = $_POST["email"];
+		$namee = $_POST["name"];
+		$emaill = $_POST["email"];
+		$id = $_POST["id"];
 
-		$updateSQL = "UPDATE test SET name='$name', email='$email' WHERE id=$id";
+		$updateSQL = "UPDATE test SET name='$namee', email='$emaill' WHERE id=$id";
+		// echo $updateSQL;
+
 		if($conn->query($updateSQL)) {
 			$message = "Updated Successfully";
 		} else {
@@ -38,10 +44,37 @@
 		$data = $fetchSingle->fetch_assoc();
 		$name = $data["name"];
 		$email = $data["email"];
+		$id = $data["id"];
 	}
 
-	$dataSQL = "SELECT * FROM test";
+	if($type == "delete") {
+		$deleteSQL = "DELETE FROM test WHERE id=$id";
+		$conn->query($deleteSQL);
+		header("location: index.php");
+	}
+
+	$searchSQL = "";
+	if($search != "") {
+		$searchSQL = " AND (name LIKE '%$search%' OR email LIKE '%$search%')";
+	}
+
+	$sortSQL = " ID ASC";
+	if($sort != ""){
+		$sort_parts = explode("-", $sort);
+		$sortSQL = $sort_parts[0]." ".$sort_parts[1];
+	}
+
+	$limitSQL = "";
+	if($limit != "") {
+		$limitSQL = " LIMIT $limit";
+	}
+
+	$dataSQL = "SELECT t.*, t2.id AS tid, t2.user_id, t2.mobile, t2.postcode FROM test t LEFT JOIN test2 t2 ON t.id=t2.user_id WHERE t.id <> 0".$searchSQL." ORDER BY ".$sortSQL.$limitSQL;
 	$data = $conn->query($dataSQL);
+
+	$totalDataSQL = "SELECT COUNT(*) AS total_records FROM test WHERE id <> 0".$searchSQL." ORDER BY ".$sortSQL.$limitSQL;
+	$totalData = $conn->query($totalDataSQL);
+	$totalRecords = $totalData->fetch_assoc();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -65,6 +98,11 @@
 		.crud_table table {
 			width: 100%;
 		}
+		.searchBox {
+			display: flex;
+    		justify-content: space-between;
+    		margin-bottom: 20px;
+		}
 	</style>
 </head>
 <body>
@@ -80,11 +118,35 @@
 				<input type="email" name="email" id="email" placeholder="Enter your email..." value="<?=$email?>" required>
 			</div>
 			<p><?=$message?></p>
+			<input type="hidden" name="id" value="<?=$id?>">
 			<input type="submit" name="<?php if($type == "edit"): echo 'updateBtn'; else: echo 'saveBtn'; endif; ?>" value="<?php if($type == "edit"): echo 'Update'; else: echo 'Save'; endif; ?> Data">
 		</form>
 	</div>
 	<div class="crud_table">
-		<h1> List of Data </h1>
+		<h1> List of Data (<?=$totalRecords["total_records"]?> Records) </h1>
+		<div class="searchBox">
+			<div class="search">
+				<input type="search" id="search" name="search" value="<?=$search?>" placeholder="Enter name or email">
+				<button type="button" onclick="search()"> Search </button>
+			</div>
+			<div class="sorting">
+				<select name="sort" id="sort" onchange="sortResults(this)">
+					<option value="ID-ASC" <?php if($sort == "ID-ASC"): echo "selected"; endif; ?>>ID ASCENDING</option>
+					<option value="ID-DESC" <?php if($sort == "ID-DESC"): echo "selected"; endif; ?>>ID DESCENDING</option>
+					<option value="CREATED_AT-ASC" <?php if($sort == "CREATED_AT-ASC"): echo "selected"; endif; ?>>DATE ASCENDING</option>
+					<option value="CREATED_AT-DESC" <?php if($sort == "CREATED_AT-DESC"): echo "selected"; endif; ?>>DATE DESCENDING</option>
+				</select>
+			</div>
+			<div class="limiting">
+				<select name="limit" id="limit" onchange="limit(this)">
+					<option value="1" <?php if($limit == "1"): echo "selected"; endif; ?>> 1 </option>
+					<option value="5" <?php if($limit == "5"): echo "selected"; endif; ?>> 5 </option>
+					<option value="10" <?php if($limit == "10"): echo "selected"; endif; ?>> 10 </option>
+					<option value="15" <?php if($limit == "15"): echo "selected"; endif; ?>> 15 </option>
+				</select>
+			</div>
+			<button onclick="location.href='index.php'"> Reset </button>
+		</div>
 		<table>
 			<thead>
 				<tr>
@@ -92,6 +154,8 @@
 					<th>NAME</th>
 					<th>EMAIL</th>
 					<th>CREATED DATE</th>
+					<th>MOBILE</th>
+					<th>POSTCODE</th>
 					<th>EDIT#</th>
 					<th>DELETE#</th>
 				</tr>
@@ -106,8 +170,10 @@
 							<td><?=$row["name"]?></td>
 							<td><?=$row["email"]?></td>
 							<td><?=$row["created_at"]?></td>
+							<td><?=$row["mobile"]?></td>
+							<td><?=$row["postcode"]?></td>
 							<td><a href="?type=edit&id=<?=$row['id']?>">EDIT</a></td>
-							<td><a href="?type=delete&id=<?=$row['id']?>">DELETE</a></td>
+							<td><a href="?type=delete&id=<?=$row['id']?>" onclick="deleteThis(this)">DELETE</a></td>
 						</tr>
 				<?php
 						endwhile;
@@ -122,5 +188,34 @@
 			</tbody>
 		</table>
 	</div>
+
+	<script>
+		function deleteThis(el) {
+			event.preventDefault();
+			if(confirm("Are you sure to delete?")) {
+				location.href = el.href;
+			}
+		}
+
+		function search() {
+			search_value = document.getElementById('search').value;
+
+			if(search_value.trim() == "") {
+				location.href = "index.php";
+			} else {
+				location.href = "index.php?search="+search_value;
+			}
+		}
+
+		function sortResults(el) {
+			sort = el.value;
+
+			location.href = "index.php?sort="+sort;
+		}
+
+		function limit(el) {
+			location.href = "index.php?limit="+el.value;
+		}
+	</script>
 </body>
 </html>
